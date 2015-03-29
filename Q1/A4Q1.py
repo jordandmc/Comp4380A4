2eaac2f22b0ab6138669e289ba5f3c52048cc73f
Morgan Epp <mrgnepp@gmail.com>
2015-03-29 00:22:50 -0500

    Q1: breadth-first printing of tree

--- Q1/A4Q1.py
+++ Q1/A4Q1.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+from collections import deque
 
 def main():
     try:
@@ -23,7 +23,7 @@ def main():
             elif op.upper() == "DELETE":
                 tree.delete(value)
             elif op.upper() == "PRINT":
-                tree.print_tree(tree.root)
+                tree.print_tree()
 
 
 def parse_input(file_name):
@@ -68,6 +68,13 @@ class BPlusTree:
         node = self.search(data)
         if node is None:
             print("\tnode is none, creating root")
+            self.root = Node()
+            self.root.keys.append(data)
+            self.root.isLeaf = True
+        elif root == node:
+            print("the root is still a leaf node")
+        else:
+            print("\tinsert data and propagate the changes up")
 
     def split(self):
         print("split")
@@ -78,9 +85,38 @@ class BPlusTree:
     def merge(self):
         print("merge")
 
-    def print_tree(self, root):
+    def print_tree(self):
         print("B+tree #" + str(self.print_num) + " with order d=" + str(self.degree))
         self.print_num += 1
+
+        if self.root is None:
+            print("Empty Root\n")
+            return
+
+        tree_string = "Root "
+        height = 0
+        queue = deque([])  # Each entry in the queue will store a list that contains [node, height]
+        queue.append([self.root, 0])
+
+        while queue:
+            queue_entry = queue.popleft()
+            node, node_height = queue_entry[0], queue_entry[1]
+            if height != node_height:
+                print("\n")
+                height += 1
+
+            if node is not None:
+                tree_string += "["
+                for key in node.keys:
+                    tree_string += " " + key
+                    if node.isLeaf:
+                        tree_string += "*"
+                tree_string += " ]  "
+
+                for data in node.data:
+                    queue.append([data, height])
+
+        print(tree_string)
 
 if __name__ == '__main__':
     main()
