4f0f89d13cc50143a1eb698b35ea0175f8dcb72b
Morgan Epp <mrgnepp@gmail.com>
2015-04-09 00:12:59 -0500

    Fixed python constructors

--- Q1/A4Q1.py
+++ Q1/A4Q1.py
@@ -21,10 +21,13 @@ def main():
             
             if op.upper() == "INSERT":
                 tree.insert(int(value))
+                print()
+                tree.print_tree()
+                print()
             elif op.upper() == "DELETE":
                 tree.delete(int(value))
-            elif op.upper() == "PRINT":
-                tree.print_tree()
+            #elif op.upper() == "PRINT":
+                #tree.print_tree()
 
 def parse_input(file_name):
     with open(file_name) as input_file:
@@ -32,9 +35,9 @@ def parse_input(file_name):
     return instructions
 
 class Node:
-    def __init__(self, keys=[], children=[], parent=None, is_leaf=None):
-        self.keys = keys  # Contains the values in index/leaf nodes
-        self.children = children  # Points to nodes below this one
+    def __init__(self, keys=None, children=None, parent=None, is_leaf=False):
+        self.keys = keys or [] # Contains the values in index/leaf nodes
+        self.children = children or []  # Points to nodes below this one
         self.parent = parent
         self.is_leaf = is_leaf
         self.has_vacancy = True
@@ -46,7 +49,7 @@ class BPlusTree:
         self.print_num = 0
     
     def evaluate_vacancy(self, node):
-        if self.degree + 1 < len(node.keys):
+        if (2 * self.degree) < len(node.keys):
             node.has_vacancy = False
     
     def search(self, value):
@@ -63,7 +66,7 @@ class BPlusTree:
                     return self.recursive_search(node.children[index], value)  # Search left
 
             # Bigger than every key in index, Go down the far right
-            return self.recursive_search(node.children[len(node.children)-1], value)
+            return self.recursive_search(node.children[len(node.children) - 1], value)
     
     def insert(self, value):
         print("insert", value)
@@ -79,46 +82,44 @@ class BPlusTree:
                 self.split(node)
 
     def split(self, node):
-        if not node.has_vacancy:
-            half = math.floor(len(node.keys)/2)
-            left_child = Node(keys=node.keys[0:half], is_leaf=node.is_leaf)
-            right_child = Node(keys=node.keys[half:len(node.keys)], is_leaf=node.is_leaf)
-            parent = node.parent
+        half = math.floor(len(node.keys) / 2)
+        left_child = Node(keys=node.keys[0:half], is_leaf=node.is_leaf)
+        right_child = Node(keys=node.keys[half:len(node.keys)], is_leaf=node.is_leaf)
+        parent = node.parent
 
-            if parent is None:
-                parent = Node(keys=[right_child.keys[0]], children=[left_child, right_child], is_leaf=False)
-                
-                if not node.is_leaf:
-                    right_child.keys.remove(right_child.keys[0])
-                self.root = parent
-            elif parent.has_vacancy:
-                parent.keys.append(right_child.keys[0])
-                parent.children.remove(node)
-                parent.children.append(left_child)
-                parent.children.append(right_child)
-                self.evaluate_vacancy(parent)
-
-                if not parent.has_vacancy:
-                    self.split(parent)
-
-            if parent.has_vacancy:
-                left_child.parent = parent
-                right_child.parent = parent
-
+        if parent is None:
+            parent = Node(keys=[node.keys[half]], children=[left_child, right_child], is_leaf=False)
+            
             if not node.is_leaf:
-                length = len(node.children)
-                majority = math.ceil(length/2)
-                left_child.children.extend(node.children[0:majority])
+                right_child.keys.remove(right_child.keys[0])
 
-                for child in left_child.children:
-                    child.parent = left_child
-                    child.children = [] #weird fix
-                right_child.children = [] #fix for weirdness of being linked to left_child.children
-                right_child.children.extend(node.children[majority:length])
+            self.root = parent
+        elif parent.has_vacancy:
+            parent.keys.append(right_child.keys[0])
+            parent.children.remove(node)
+            parent.children.append(left_child)
+            parent.children.append(right_child)
+            self.evaluate_vacancy(parent)
 
-                for child in right_child.children:
-                    child.parent = right_child
-                    child.children = [] #weird fix
+            if not parent.has_vacancy:
+                self.split(parent)
+
+        if parent.has_vacancy:
+            left_child.parent = parent
+            right_child.parent = parent
+
+        if not node.is_leaf:
+            length = len(node.children)
+            majority = math.ceil(length / 2)
+            left_child.children.extend(node.children[0:majority])
+
+            for child in left_child.children:
+                child.parent = left_child
+
+            right_child.children.extend(node.children[majority:length])
+
+            for child in right_child.children:
+                child.parent = right_child
 
     def delete(self, value):
         print("delete", value)
@@ -240,7 +241,6 @@ class BPlusTree:
                 height += 1
 
             if node is not None:
-                
                 tree_string += "["
                 for key in node.keys:
 
